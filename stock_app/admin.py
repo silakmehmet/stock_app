e1397c1b023f4b82ea50b6f8d7f16b919b8e4c9b
Mehmet Şılak <silakmehmet@gmail.com>
2024-09-04 13:20:27 +0300

    price_total field excluded from admin panel

--- stock_app/admin.py
+++ stock_app/admin.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from .models import Category, Firm, Product, Purchases, Sales, Brand
 
 
+class PurchasesAdmin(admin.ModelAdmin):
+    exclude = ['price_total']
+
+
+class SalesAdmin(admin.ModelAdmin):
+    exclude = ['price_total']
+
+
 admin.site.register(Category)
 admin.site.register(Firm)
 admin.site.register(Product)
-admin.site.register(Purchases)
-admin.site.register(Sales)
+admin.site.register(Purchases, PurchasesAdmin)
+admin.site.register(Sales, SalesAdmin)
 admin.site.register(Brand)
