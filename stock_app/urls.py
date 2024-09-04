b1c8180abdde363b9e775a924d095b8fcd9a0bab
Mehmet Şılak <silakmehmet@gmail.com>
2024-09-04 12:36:15 +0300

    Views created with endpoints

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryMVS, FirmMVS, BrandMVS, ProductMVS, PurchasesMVS, SalesMVS
+
+router = DefaultRouter()
+
+router.register("categories/", CategoryMVS)
+router.register("firms/", FirmMVS)
+router.register("brands/", BrandMVS)
+router.register("products/", ProductMVS)
+router.register("purchases/", PurchasesMVS)
+router.register("sales/", SalesMVS)
+
 
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
--- stock_app/views.py
+++ stock_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category, Firm, Product, Purchases, Sales, Brand
+from .serializers import CategorySerializer, FirmSerializer, ProductSerializer, PurchasesSerializer, SalesSerializer, BrandSerializer
+
+
+class CategoryMVS(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class FirmMVS(ModelViewSet):
+    queryset = Firm.objects.all()
+    serializer_class = FirmSerializer
+
+
+class ProductMVS(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class PurchasesMVS(ModelViewSet):
+    queryset = Purchases.objects.all()
+    serializer_class = PurchasesSerializer
+
+
+class SalesMVS(ModelViewSet):
+    queryset = Sales.objects.all()
+    serializer_class = SalesSerializer
+
+
+class BrandMVS(ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
