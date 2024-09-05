fe93b638fb0e3b6df497b63dd967d4bcf9c70a97
Mehmet Şılak <silakmehmet@gmail.com>
2024-09-05 14:52:01 +0300

    DjangoFilterBackend added for filtering

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -10,9 +12,10 @@ from .mixins import UserMixin
 class CategoryMVS(UserMixin, ModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ["name"]
     ordering_fields = ["name"]
+    filterset_fields = ["name"]
 
     def get_serializer_class(self):
         if "search" in self.request.query_params:
@@ -32,27 +35,30 @@ class ProductMVS(UserMixin, ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ["name"]
     ordering_fields = ["category", "brand"]
+    filterset_fields = ["category", "brand"]
 
 
 class PurchasesMVS(UserMixin, ModelViewSet):
     queryset = Purchases.objects.all()
     serializer_class = PurchasesSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ["firm"]
     ordering_fields = ["firm", "product"]
+    filterset_fields = ["firm", "product"]
 
 
 class SalesMVS(UserMixin, ModelViewSet):
     queryset = Sales.objects.all()
     serializer_class = SalesSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ["firm"]
-    ordering_fields = ["firm", "product"]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ["brand"]
+    ordering_fields = ["brand", "product"]
+    filterset_fields = ["brand", "product"]
 
 
 class BrandMVS(UserMixin, ModelViewSet):
